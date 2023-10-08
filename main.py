34aa754e942a6e024e1e131233be68b5225ba6aa
Maria Ignateva <mariya.ignateva.01@mail.ru>
2023-10-08 13:42:04 +0300

    небольшой комментарий

--- main.py
+++ main.py
@@ -61,6 +61,6 @@ async def read_items() -> list[Item]: # тут мы не принимаем ни
 
 
 fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
-@app.get("/fake_items/")
+@app.get("/fake_items/") #http://127.0.0.1:8000/items/?skip=0&limit=10
 async def read_item(skip: int = 0, limit: int = 10):
     return fake_items_db[skip: skip + limit]
